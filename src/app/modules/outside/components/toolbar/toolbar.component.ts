7e68feb4caa3fa6738c4b4b886102a1b2d773212
guus7avo <ps.gustavo19@gmail.com>
2021-07-30 18:23:48 -0300

    teste deploy automático git

--- src/app/modules/outside/components/toolbar/toolbar.component.ts
+++ src/app/modules/outside/components/toolbar/toolbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  title = 'sistema-livros';
+  title = 'Marcador de Livros';
   isAuthenticated = false;
 
   constructor() { }
