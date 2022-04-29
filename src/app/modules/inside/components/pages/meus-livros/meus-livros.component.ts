bbab71596f8574af9ba0acd5067b8e20de5a4adb
Gustavo <ps.gustavo19@gmail.com>
2022-04-28 22:25:51 -0300

    melhoria na tabela de exibição de livros

--- src/app/modules/inside/components/pages/meus-livros/meus-livros.component.ts
+++ src/app/modules/inside/components/pages/meus-livros/meus-livros.component.ts
@@ -11,39 +11,19 @@ import { Livro } from 'src/app/core/services/models/livro.models';
 import { EditarLivroComponent } from '../../dialogs/editar-livro/editar-livro.component';
 import { DeletarLivroComponent } from '../../dialogs/deletar-livro/deletar-livro.component';
 
-
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  fruit: string;
-}
-
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry', 'lychee', 'kiwi', 'mango', 'peach', 'lime', 'pomegranate', 'pineapple'
-];
-const NAMES: string[] = [
-  'Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack', 'Charlotte', 'Theodore', 'Isla', 'Oliver',
-  'Isabella', 'Jasper', 'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'
+const ELEMENT_DATA: Livro[] = [
+  {id: '', titulo: '', autor: '', genero: ''},
 ];
 
-/**
- * @title Data table with sorting, pagination, and filtering.
- */
  @Component({
   selector: 'app-meus-livros',
   templateUrl: './meus-livros.component.html',
   styleUrls: ['./meus-livros.component.scss']
 })
-export class MeusLivrosComponent implements AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+export class MeusLivrosComponent {
 
-  @ViewChild(MatPaginator)
-   paginator!: MatPaginator;
-  @ViewChild(MatSort)
-   sort!: MatSort;
+  displayedColumns: string[] = [ 'titulo', 'autor', 'genero', 'editar', 'excluir'];
+  dataSource = ELEMENT_DATA;
 
    formLivro: FormGroup;
 
@@ -57,11 +37,6 @@ export class MeusLivrosComponent implements AfterViewInit {
       genero: ['', Validators.compose([Validators.required])]
     })
 
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
   }
 
   // openDialog() {
@@ -131,30 +106,5 @@ export class MeusLivrosComponent implements AfterViewInit {
       })
   }
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
 }
 
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name = NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))]
-  };
-}
