98ead1e55d3ca0d0dbbd050db8b8f71ac78274e7
Gustavo <ps.gustavo19@gmail.com>
2022-05-19 18:43:12 -0300

    melhorias na landing page

--- src/app/modules/outside/components/pages/welcome/welcome.component.ts
+++ src/app/modules/outside/components/pages/welcome/welcome.component.ts
@@ -13,21 +13,16 @@ export class WelcomeComponent {
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'Card 1', cols: 1, rows: 1 },
-          { title: 'Card 2', cols: 1, rows: 1 },
-          { title: 'Card 3', cols: 1, rows: 1 },
-          { title: 'Card 4', cols: 1, rows: 1 }
+          {cols: 2, rows: 1,
+          }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+        {cols: 1, rows: 1,
+        }
       ];
     })
   );
-
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
